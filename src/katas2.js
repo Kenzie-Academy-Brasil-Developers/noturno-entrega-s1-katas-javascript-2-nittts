0b3afa6e3de34800d628ceba032c1cac89856004
Nittts <w.baierle@gmail.com>
2021-10-20 22:11:18 -0300

    Atividade finalizada

--- src/katas2.js
+++ src/katas2.js
@@ -48,17 +48,17 @@ function factorial(num) {
 
 // crie a função fibonacci
 function fibonacci(num) {
-    let num1 = 0
-    let num2 = 1
+    let num1 = 1
+    let num2 = 0
     let resultado = 0
     for(let contador = 1;contador < num; contador++ ) {
-        console.log(num1)
+        console.log(num2)
         resultado = add(num1,num2)
         num1 = num2
         num2 = resultado    
     }
     return resultado
 }
-console.log(fibonacci(10))
+console.log(fibonacci(5))
 // descomente a linha seguinte para testar sua função
 // console.assert(fibonacci(8) === 13, 'A função fibonacci não está funcionando como esperado');
